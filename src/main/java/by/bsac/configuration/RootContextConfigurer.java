57ab2adaf2d9a9cd17ffe2a0ea0bf2ac5e5e22e1
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-05-24 12:57:24 +0300

    Root application context configuration class.

--- src/main/java/by/bsac/configuration/RootContextConfigurer.java
+++ src/main/java/by/bsac/configuration/RootContextConfigurer.java
@@ -1,26 +1,14 @@
 package by.bsac.configuration;
 
 import by.bsac.models.HtmlFormsBeans;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-
 
 @Configuration
 @Import(value = {HtmlFormsBeans.class,
                                 by.bsac.configuration.PersistenceConfiguration.class})
-@PropertySource(value = "classpath:application.properties")
 public class RootContextConfigurer {
 
-    //Spring beans
-    private Environment spring_environment;
 
-
-    //Spring autowiring
-    @Autowired
-    public void autowire(Environment a_environment) {
-        this.spring_environment = a_environment;
-    }
 }
